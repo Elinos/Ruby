339261efa19bb7552d6bf4110017208556dfde84
Elinos <elinos@gmail.com>
2013-10-01 23:35:52 +0300

    ruby_warrior

---
+++ Learning/ruby_warrior.rb
@@ -0,0 +1,37 @@
+class Player
+  @hit_wall =  false
+  @pivot = false
+  def play_turn(warrior)
+    unless @hit_wall
+      if warrior.feel(:backward).captive?
+        warrior.rescue! :backward
+      elsif warrior.feel(:backward).enemy?
+        unless @pivot
+          warrior.pivot!
+          @pivot = true
+        else
+          warrior.attack!
+        end
+      elsif warrior.health < 20 && @health <= warrior.health
+        warrior.rest!
+      elsif warrior.feel(:backward).empty?
+        warrior.walk! :backward
+      elsif warrior.feel(:backward).wall?
+        @hit_wall = true;
+      end
+    else
+      if warrior.feel.captive?
+        warrior.rescue!
+      elsif warrior.feel.enemy?
+        warrior.attack!
+      elsif @health > warrior.health && warrior.health < 10
+        warrior.walk! :backward
+      elsif warrior.health < 20 && @health <= warrior.health
+        warrior.rest!
+      elsif warrior.feel.empty?
+        warrior.walk!
+      end
+    end
+    @health = warrior.health
+  end
+end
